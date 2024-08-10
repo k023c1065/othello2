47a5f58155830447b6fe42d23a739f3295025189
k023c1065 <k023c1065@g.neec.ac.jp>
2024-08-10 19:38:41 +0900

    fix

--- get_exp.py
+++ get_exp.py
@@ -44,6 +44,11 @@ class exp_memory_class:
             process.start()
         self.model_executer(game_num,proc_num)
 
+        for process in self.processes:
+            print("Join process")
+            process.join()
+            process.close()
+            
         for pipe in result_pipe:
             exp,win_score = pipe[1].recv()
             tmp_exp += exp
@@ -53,23 +58,20 @@ class exp_memory_class:
         self.latest_memory = tmp_exp
         print(f"Final win_score:{tmp_score}")
 
-        # for pipe in self.pipes:
+        for pipe in self.pipes:
             
-        #     pipe[0].close()
-        #     del pipe[0]
-        #     pipe[1].close()
-        #     del pipe[1]
-        # for pipe in result_pipe:
-        #     pipe[0].close()
-        #     del pipe[0]
-        #     pipe[1].close()
-        #     del pipe[1]
+            pipe[0].close()
+            del pipe[0]
+            pipe[1].close()
+            del pipe[1]
+        for pipe in result_pipe:
+            pipe[0].close()
+            del pipe[0]
+            pipe[1].close()
+            del pipe[1]
         
         self.pipes = []
-        for process in self.processes:
-            print("Join process")
-            process.join()
-            process.close()
+
         return tmp_score
     def model_executer(self,num,proc_num):
         num = 60*num
